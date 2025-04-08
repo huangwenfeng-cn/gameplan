77ab54d0cf878414a401ae6aca0fb641113b017c
huangwenfeng-cn <134998561+huangwenfeng-cn@users.noreply.github.com>
2025-04-08 12:49:23 +0800

    Update Notifications.vue

--- frontend/src/pages/Notifications.vue
+++ frontend/src/pages/Notifications.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="sticky top-0 z-10 border-b bg-surface-white px-4 py-2.5 sm:px-5">
     <div class="flex items-center justify-between">
-      <Breadcrumbs :items="[{ label: 'Notifications', route: { name: 'Notifications' } }]" />
+      <Breadcrumbs :items="[{ label: '通知', route: { name: 'Notifications' } }]" />
       <div class="flex h-7 items-center space-x-2">
         <Button
           @click="$resources.markAllAsRead.submit"
@@ -11,7 +11,7 @@
           Mark all as read
         </Button>
         <TabButtons
-          :buttons="[{ label: 'Unread', active: true }, { label: 'Read' }]"
+          :buttons="[{ label: '未读', active: true }, { label: '已读' }]"
           v-model="activeTab"
         />
       </div>
